57d253b5233c0220831d37911d30276b728d7d54
Kew <wolff.kevin@protonmail.com>
2026-03-04 18:38:18 +0100

    UI: improve header responsive

--- wp-content/themes/my-theme/partials/site-header.php
+++ wp-content/themes/my-theme/partials/site-header.php
@@ -5,11 +5,11 @@
 			if (has_custom_logo()) {
 				the_custom_logo();
 			} else {
-				?>
+			?>
 				<a class="site-header__brand-link" href="<?php echo esc_url(home_url('/')); ?>">
 					<img class="site-header__logo" src="<?php echo esc_url(get_theme_file_uri('assets/images/logo_aimer_guerir.png')); ?>" alt="Aimer Gu&eacute;rir">
 				</a>
-				<?php
+			<?php
 			}
 			?>
 			<p class="site-header__brand-tagline"><?php echo esc_html(get_theme_mod('my_theme_contact_tagline', 'Magnétiseur – Coupeur de Feu')); ?></p>
@@ -19,7 +19,7 @@
 				<span class="site-header__contact-label">T&eacute;l&eacute;phone</span>
 				<a class="site-header__contact-value" href="tel:<?php echo esc_attr(str_replace(' ', '', get_theme_mod('my_theme_contact_phone', '06 95 64 54 76'))); ?>"><?php echo esc_html(get_theme_mod('my_theme_contact_phone', '06 95 64 54 76')); ?></a>
 			</div>
-			<div class="site-header__contact-item">
+			<div class="site-header__contact-item site-header__contact-item--email">
 				<span class="site-header__contact-label">E-mail</span>
 				<a class="site-header__contact-value" href="mailto:<?php echo esc_attr(get_theme_mod('my_theme_contact_email', 'contact@aimerguerir.com')); ?>"><?php echo esc_html(get_theme_mod('my_theme_contact_email', 'contact@aimerguerir.com')); ?></a>
 			</div>
@@ -44,25 +44,22 @@
 					'fallback_cb' => false,
 				]);
 			} else {
-				?>
+			?>
 				<ul class="site-nav__list">
 					<li class="site-nav__item">
 						<a href="<?php echo esc_url(home_url('/')); ?>">Accueil</a>
 					</li>
 					<li class="site-nav__item">
-						<a href="<?php echo esc_url(home_url('/prendre-rendez-vous')); ?>">Rendez-vous</a>
-					</li>
-					<li class="site-nav__item">
-						<a href="<?php echo esc_url(home_url('/les-seances/')); ?>">Les s&eacute;ances</a>
-					</li>
-					<li class="site-nav__item">
 						<a href="<?php echo esc_url(home_url('/qui-suis-je/')); ?>">Qui suis-je</a>
+					</li>
+					<li class="site-nav__item">
+						<a href="<?php echo esc_url(home_url('/prendre-rendez-vous')); ?>">Prendre rendez-vous</a>
 					</li>
 					<li class="site-nav__item">
 						<a href="<?php echo esc_url(home_url('/blog/')); ?>">Articles</a>
 					</li>
 				</ul>
-				<?php
+			<?php
 			}
 			?>
 		</nav>
